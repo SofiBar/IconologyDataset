07f24912bbd0b1f5deeaefba0f7bd38da8fb00ed
SofiBar <74154570+SofiBar@users.noreply.github.com>
2022-09-12 18:26:37 +0200

    Update tryPanofsky.py

--- tryPanofsky.py
+++ tryPanofsky.py
@@ -5,7 +5,7 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 # !pip install graphviz
-# import graphviz as graphviz
+# import graphviz as graphviz # not working
 
 st.title("Data analysis of the Panofsky dataset")
 
@@ -59,10 +59,34 @@ c = alt.Chart(df4).mark_circle().encode(
 st.altair_chart(c, use_container_width=True)
 
 
-#st.heading("The graph visualized")
+#st.heading("The graph visualized") 
 
 # Create a graphlib graph object
 #graph = graphviz.Digraph()
 #graph.parse("210622_output2.ttl", format="ttl")
 
 #st.graphviz_chart(graph)
+st.write("Here we try to visualize some data queried from the Blazegraph endpoint")
+
+server = sparql.SPARQLServer('http://127.0.0.1:9999/bigdata/sparql')
+
+# Loading data to Blazegraph
+server.update('load <file:///210622_output2.ttl>')
+people_result = []
+# Executing query
+result = server.query(
+         """PREFIX d: <http://icondataset.org/> 
+PREFIX icon: <https://w3id.org/icon/ontology/>
+PREFIX crm: <http://www.cidoc-crm.org/cidoc-crm/>
+
+SELECT (count(distinct ?people) as ?tot) WHERE {
+
+?people a crm:E21_Person. 
+
+}""")
+for b in result['results']['bindings']:
+    people_result.append(b["people"]["value"])
+d = {'col1': "People", 'col2': people_result[0]}
+df5 = pd.DataFrame(d)    
+
+st.write(df5)
